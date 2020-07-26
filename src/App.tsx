271f21f0d6db9222c9ec0d9f9298d13086a735ef
daRong91 <k626541201@hotmail.com>
2020-07-26 13:54:52 +0800

    project tool settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button } from 'antd';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Button } from 'antd'
+import LoadLocal from './LoadLocal'
 
 function App() {
   return (
@@ -11,18 +12,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
         <Button type="primary">Button</Button>
+        <LoadLocal />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/LoadLocal.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const LoadLocal: React.FC = () => {
+  return <>I'm local</>
+}
+export default LoadLocal
