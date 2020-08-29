87b643dd7a5e5ae90122182b0603da0dc718e8f5
daRong91 <k626541201@hotmail.com>
2020-08-29 15:03:08 +0800

    Add new home page

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,53 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css'
-import { Layout, message } from 'antd'
-import axios from 'axios'
-import MdContent from '../components/Content/MdContent'
-import AppHeader from '../components/Header'
-import AppSidebar from '../components/Sidebar'
-import BreadCrumbHeader from '../components/BreadCrumbHeader'
+import Home from '../Home/Home'
 
 const App: React.FC = () => {
-  const [currentItemPath, setCurrentItemPath] = useState('')
-  const [refreshSideBarCount, setRefreshSideBarCount] = useState(0)
-  const [mdContent, setMdContent] = useState('')
-  const postMdContent = (fileFullPath: string): void => {
-    axios.post('./api/files/upload', { name: fileFullPath, mdContent }).then((res) => {
-      if (res.status === 201) {
-        setCurrentItemPath(fileFullPath)
-        setRefreshSideBarCount(refreshSideBarCount + 1)
-      }
-      message.success(res.statusText)
-    })
-  }
-  return (
-    <Layout>
-      <AppHeader />
-      <Layout>
-        <AppSidebar
-          currentItemPath={currentItemPath}
-          setCurrentItem={setCurrentItemPath}
-          refreshSideBarCount={refreshSideBarCount}
-        />
-        <Layout style={{ padding: '0 24px 24px' }}>
-          <BreadCrumbHeader
-            currentItemPath={currentItemPath}
-            saveItem={(path: string): void => {
-              postMdContent(path)
-            }}
-            addNewItem={(): void => {
-              setCurrentItemPath(currentItemPath.split('/')[0])
-            }}
-          />
-          <MdContent
-            currentItemPath={currentItemPath}
-            returnNewMdContent={(value: string): void => {
-              setMdContent(value)
-            }}
-          />
-        </Layout>
-      </Layout>
-    </Layout>
-  )
+  return <Home />
 }
 export default App
---
+++ src/Home/Home.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Row, Col } from 'antd'
+import styled from 'styled-components'
+
+const Banner = styled.div`
+  background-image: url(${process.env.PUBLIC_URL}/anime-girl.jpeg);
+  background-size: cover;
+  height: 640px;
+  background-position-y: -120px;
+`
+const Divider = styled.div`
+  border-bottom: 1px solid #ebebeb;
+  margin-bottom: 16px;
+  padding-bottom: 8px;
+`
+
+type itemType = { [key: string]: string[] }
+interface DirectoryType {
+  [key: string]: Array<itemType>
+}
+const Home: React.FC = () => {
+  const [fileDirs, setFileDirs] = useState({})
+  useEffect(() => {
+    axios
+      .get(`./api/files`)
+      .then((res) => {
+        setFileDirs(res.data)
+      })
+      .catch((err) => {})
+  }, [])
+
+  //   const [categoryName, categoryDirs] = Object.entries(fileDirs)
+  return (
+    <>
+      <Banner />
+      <Row justify="center" style={{ marginTop: '20px' }}>
+        <Col span={16}>
+          {Object.entries(fileDirs as DirectoryType).map(([categoryName, categoryDirs]) => (
+            <div key={categoryName} style={{ marginTop: '40px' }}>
+              <h2 style={{ marginBottom: '40px' }}>{categoryName}</h2>
+              {categoryDirs.map((categoryDir) =>
+                Object.entries(categoryDir).map(([fileDirName, fileNames]) => (
+                  <section
+                    key={fileDirName}
+                    className="section"
+                    style={{ marginBottom: '24px', counterIncrement: 'a' }}
+                  >
+                    <h3>{fileDirName}</h3>
+                    <Divider />
+                    <ul className="list">
+                      {fileNames.map((fileName: string) => (
+                        <li className="list_item" key={fileName}>
+                          <div>
+                            <a href=":;">{fileName}</a>
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
+                  </section>
+                ))
+              )}
+            </div>
+          ))}
+        </Col>
+      </Row>
+    </>
+  )
+}
+export default Home
