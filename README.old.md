49b7df1f4ad99f5add2bb1cc63820e8a01ffe900
daRong91 <k626541201@hotmail.com>
2020-09-21 23:02:59 +0800

    Add sass

--- README.old.md
+++
@@ -1,5 +0,0 @@
-# darong91.github.io
-Hello
-
-* [Algorithm](docs/algorithm/index.md)
-* [React](docs/react/index.md)
--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './App.css'
+import './App.scss'
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import Home from '../Pages/Home'
 import Docs from '../Pages/Docs'
