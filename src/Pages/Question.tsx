73a913d193b78c74718233e0df971e7f3922e416
OhNaNaSun <k626541201@hotmail.com>
2021-07-14 23:32:05 +0800

    use accordin for doc list

--- src/Pages/Question.tsx
+++ src/Pages/Question.tsx
@@ -3,18 +3,49 @@ import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
 import Typography from '@material-ui/core/Typography'
 import Box from '@material-ui/core/Box'
-import List from '@material-ui/core/List'
-import ListItem, { ListItemProps } from '@material-ui/core/ListItem'
 import Button from '@material-ui/core/Button'
+import * as Showdown from 'showdown'
+import Accordion from '@material-ui/core/Accordion'
+import AccordionSummary from '@material-ui/core/AccordionSummary'
+import AccordionDetails from '@material-ui/core/AccordionDetails'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
 
 type QuestionMapType = { content: string; title: string }[]
-
+const converter = new Showdown.Converter({
+  tables: true,
+  simplifiedAutoLink: true,
+  strikethrough: true,
+  tasklists: true,
+})
 const tabContentMap = [
   { key: 'javascript', name: 'JavaScript' },
   { key: 'html', name: 'HTML' },
   { key: 'css', name: 'CSS' },
 ]
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+    },
+    heading: {
+      fontSize: theme.typography.pxToRem(15),
+      flexBasis: '33.33%',
+      flexShrink: 0,
+    },
+    secondaryHeading: {
+      fontSize: theme.typography.pxToRem(15),
+      color: theme.palette.text.secondary,
+    },
+  })
+)
 const QuestionPage: React.FC = () => {
+  const classes = useStyles()
+  const [expanded, setExpanded] = React.useState<string | false>(false)
+
+  const handleChangeExpand = (panel: string) => (event: React.ChangeEvent<{}>, isExpanded: boolean) => {
+    setExpanded(isExpanded ? panel : false)
+  }
   const [tabIndex, setTabIndex] = React.useState(0)
   const [questionList, setQuestionList] = useState<QuestionMapType | null>(null)
   useEffect(() => {
@@ -37,15 +68,21 @@ const QuestionPage: React.FC = () => {
       </Tabs>
       <div role="tabpanel">
         <Box p={3}>
-          <List>
+          <div>
             {questionList?.map(({ title, content }, index) => (
-              <ListItem key={index}>
-                <Typography>
-                  {title}: {content}
-                </Typography>
-              </ListItem>
+              <Accordion key={index} expanded={expanded === 'panel1'} onChange={handleChangeExpand('panel1')}>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1bh-content" id="panel1bh-header">
+                  <Typography className={classes.heading}>{title}</Typography>
+                  <Typography className={classes.secondaryHeading}>I am an accordion</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    <span dangerouslySetInnerHTML={{ __html: converter.makeHtml(content) }} />
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
             ))}
-          </List>
+          </div>
           <Button target="_blank" href={`${tabContentMap[tabIndex].key}/edit`} color="secondary">
             + Add
           </Button>
