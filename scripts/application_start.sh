0fd116f92a7e7b785a00e4b9bf7f4ad217505018
OhNaNaSun <k626541201@hotmail.com>
2022-01-19 11:19:53 +0800

    Update deploy shell

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -11,6 +11,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
+# kill
+npx kill-port 3000
+
 #install node modules
 npm install
 
--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #Stopping existing node servers
 echo "Stopping any existing node servers"
-kill -9 $(lsof -t -i:3000)
